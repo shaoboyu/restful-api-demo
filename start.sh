1b81ca4880150c4f0eeacc00b1137094e72c00bf
yushaobo1996@outlook.com <yushaobo521>
2020-03-31 21:03:41 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/git/bin
-pwd
+
 nohup java -jar /home/restful-api-demo/target/restful-api-demo-0.0.1-SNAPSHOT.jar &
