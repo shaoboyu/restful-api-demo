89ad63e1b2218d7edf6e0f2d8e8d60cccb5103ac
yushaobo1996@outlook.com <yushaobo521>
2020-03-31 20:36:20 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo 'JAVA_HOME is $JAVA_HOME'
 echo 'PATH is $PATH'
-source /etc/profile
 nohup java -jar /home/restful-api-demo/target/restful-api-demo-0.0.1-SNAPSHOT.jar &  >>/home/log.txt
